c253fdda026b7a1b3f5d4fd1a22dc74cddb71f37
FE Tony <fe.tony@example.com>
2025-12-16 14:56:35 +0700

    revert lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,10 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'http://thanktoanf.online',
-	output: 'server',
-	adapter: vercel({
-		imageService: true,
-	}),
 	integrations: [mdx(), sitemap()],
-	// Using output: "server" to enable SSR for pages with prerender = false
-	// Pages without prerender = false will still be statically generated during build
 });
